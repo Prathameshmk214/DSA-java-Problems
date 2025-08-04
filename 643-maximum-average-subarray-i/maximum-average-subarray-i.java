9e4e5a4f88bfddf2e406584b0e6fc4a7aa300c13
Prathamesh Kulkarni <167446877+Prathameshmk214@users.noreply.github.com>
2025-08-04 09:16:16 +0530

    Time: 2 ms (99.88%) | Memory: 56 MB (87.56%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -4,8 +4,9 @@ class Solution {
         int maxSum = 0 ;
         for(int i = 0 ; i< k ; i++){
             currSum += nums[i];
-            maxSum = currSum ;
+            
         }
+        maxSum = currSum ;
         for(int i = k ; i< nums.length; i++){
             currSum += nums[i] - nums[i-k];
             if(currSum > maxSum){
